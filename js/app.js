50ad117611a9635b584405dcd08dab106633aebe
ttom-eragiapreso <109075301+ttom-eragiapreso@users.noreply.github.com>
2022-10-26 17:59:48 +0200

    bottoni implementati bene con scorrimento foto

--- js/app.js
+++ js/app.js
@@ -1,23 +1,3 @@
-// **Consegna:**
-// Riprendiamo l’esercizio carosello e rifacciamolo, questa volta usando un array di oggetti.
-// Ogni elemento deve avere un titolo, una descrizione e il riferimento ad una immagine.
-// Immagini proposte (non obbligatorie):
-// Svezia http://www.viaggiareonline.it/wp-content/uploads/2014/11/sweden_148857365.jpg
-// Perù https://static1.evcdn.net/images/reduction/1513757_w-1920_h-1080_q-70_m-crop.jpg
-// Chile https://img.itinari.com/pages/images/original/0d3ed180-d22d-48e8-84df-19c4d888b41f-62-crop.jpg?ch=DPR&dpr=2.625&w=1600&s=7ebd4b5a9e045f41b4e0c7c75d298d6c
-// Argentina https://static1.evcdn.net/images/reduction/1583177_w-1920_h-1080_q-70_m-crop.jpg
-// Colombia https://cdn.sanity.io/images/24oxpx4s/prod/ed09eff0362396772ad50ec3bfb728d332eb1c30-3200x2125.jpg?w=1600&h=1063&fit=crop
-// Le immagini devono essere 5 e nella grafica devono essere presenti:
-// - immagine in evidenza
-// - thumbnail di tutte le immagine con in evidenza l’immagine attiva
-// - bottone avanti e indietro
-// Lo screenshot allegato è solo dimostrativo ma cercate di mettere le thumb in basso e non laterlamente
-// **Bonus 1:**
-// Sperimentiamo attraverso l’uso delle timing functions anche una funzionalità di scorrimento al nostro carosello:al click di un bottone o già dall’inizio possiamo far partire, ad intervalli di tempo a piacere, lo scorrimento delle immagini disponibili nel carosello stesso.
-// ****
-// **Bonus 2:**
-// E se volessi un bottone per invertire la “direzione” del carosello?
-// ****
 
 console.log("app loaded")
 
@@ -50,8 +30,10 @@ const mainWrapper = document.querySelector(".main-wrapper");
 const carousel = document.querySelector(".carousel");
 const slider = document.querySelector(".slider");
 const thumbnails = document.querySelector(".thumbnails");
+const prevBtn = document.querySelector(".prev");
+const nextBtn = document.querySelector(".next");
 let counter = 0;
-let autplayCounter = 0;
+let autoplayCounter = 0;
 
 
 // Aggiungo tutte le foto al DOM, sia nello slilder principale che nel thumbnails
@@ -73,14 +55,51 @@ addIndex();
 
 
 
+nextBtn.addEventListener("click", function(){
+  nextPrev(true)
+})
+
+prevBtn.addEventListener("click", function(){
+  nextPrev(false)
+})
 
 
 
 
 
 
+function nextPrev(isNext){
+
+  let photoCurrentlyShown = allHiddenImgContainer.filter( photo => !photo.classList.contains("d-none"))
+  photoCurrentlyShown[0].classList.add("d-none");
+  
+  if(isNext){
 
 
+    if(autoplayCounter +1 === allPhotosInThumbnail.length){
+      allPhotosInThumbnail[autoplayCounter].classList.remove("active")
+      autoplayCounter =  0;
+      allPhotosInThumbnail[autoplayCounter].classList.add("active");
+      allHiddenImgContainer[autoplayCounter].classList.remove("d-none")
+    }else {
+      allPhotosInThumbnail[autoplayCounter].classList.remove("active")
+      allPhotosInThumbnail[++autoplayCounter].classList.add("active")
+      allHiddenImgContainer[autoplayCounter].classList.remove("d-none")
+    }
+  }else {
+
+    if(autoplayCounter === 0){
+      allPhotosInThumbnail[autoplayCounter].classList.remove("active")
+      autoplayCounter = allPhotosInThumbnail.length -1;
+      allPhotosInThumbnail[autoplayCounter].classList.add("active");
+      allHiddenImgContainer[autoplayCounter].classList.remove("d-none")
+    }else {
+      allPhotosInThumbnail[autoplayCounter].classList.remove("active")
+      allPhotosInThumbnail[--autoplayCounter].classList.add("active")
+      allHiddenImgContainer[autoplayCounter].classList.remove("d-none")
+    }
+  }
+}
 
 function addIndex(){
 
@@ -100,7 +119,7 @@ function addIndex(){
 function showOnClick(){
   //Mi prendo il container e la foto del thumbnail sul quale ho cliccato andando a filtrare l'array di container basandomi sull'indice che avevo assegnato come proprietà custom al momento dell'iniezione nel DOM
   let photoContainerClicked = allHiddenImgContainer.filter( photo => photo.index === this.index);
-  let photoThumbnailClicked = allPhotosInThumbnail.filter( photo => photo.index === this.index)
+  let photoThumbnailClicked = allPhotosInThumbnail.filter( photo => photo.index === this.index);
 
   //Se il container che ho cliccato non contiene la classe d-none, vuol dire che è quello che in quel momento è attivo, quindi non faccio nulla.
   if(!photoContainerClicked[0].classList.contains("d-none")){
@@ -117,8 +136,7 @@ function showOnClick(){
     photoContainerClicked[0].classList.remove("d-none")
     photoThumbnailClicked[0].classList.add("active")
   }
-
-
+  autoplayCounter = this.index
 }
 
 function addImagesToDom(){
